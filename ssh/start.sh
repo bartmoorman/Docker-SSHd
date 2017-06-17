dd24ce31feccb9dcff9f91baa16df77432dfad6e
Bart Moorman <bmoorman@Hydra.id10t.us>
2017-06-16 23:09:02 -0600

    Okay. We won't include an anchor... Picky SoB.

--- ssh/start.sh
+++ ssh/start.sh
@@ -19,7 +19,7 @@ if [ ! -f /var/log/denyhost ] && [ ! -f /var/lib/denyhosts/offset ]; then
     sed -ri \
     -e 's/^(IPTABLES\s+=\s+.*)/#\1/' \
     -e 's/^(ADMIN_EMAIL\s+=\s+.*)/#\1/' \
-    -e '/^#USERDEF_FAILED_ENTRY_REGEX/!b;n;cUSERDEF_FAILED_ENTRY_REGEX = Failed (?P<method>\\S*) for (?P<invalid>invalid user |illegal user )?(?P<user>.*) from (::ffff:)?(?P<host>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})( port \\d+)?( ssh2)?$' \
+    -e '/^#USERDEF_FAILED_ENTRY_REGEX/!b;n;cUSERDEF_FAILED_ENTRY_REGEX = Failed (?P<method>\\S*) for (?P<invalid>invalid user |illegal user )?(?P<user>.*) from (::ffff:)?(?P<host>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})( port \\d+)?( ssh2)?' \
     /etc/denyhosts.conf
 fi
 
